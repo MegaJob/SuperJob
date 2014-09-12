2145a216d5a630af88c7322fc3a55481a530b612
Lev Taydakov <lev.taydakov@gmail.com>
2014-09-11 22:43:47 -0700

    memdb enhancment

--- memdb/index.js
+++ memdb/index.js
@@ -26,3 +26,21 @@ exports.first = function(obj) {
 exports.clear = function() {
 	db = [];
 };
+
+if (!Function.prototype.bind) {
+	Function.prototype.bind=function(b){
+		if(typeof this!=="function"){
+			throw new TypeError("Function.prototype.bind - what is trying to be bound is not callable");
+		}
+		function c(){}
+		var a=[].slice,
+			f=a.call(arguments,1),
+			e=this,
+			d=function(){
+				return e.apply(this instanceof c?this:b||window,f.concat(a.call(arguments)));
+			};
+			c.prototype=this.prototype;
+			d.prototype=new c();
+		return d;
+	};
+}

323c30ffcdbf562c9de16093bccc9418dafe22b6
Lev Taydakov <lev.taydakov@gmail.com>
2014-09-13 18:50:57 -0700

    User model implementation. STEP 1

--- models/user.js
+++ models/user.js
@@ -1,5 +1,11 @@
+var _ = require('nimble');
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+
+mongoose.connect('mongodb://localhost/mytest');
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
 
 var userSchema = new Schema({
   id: Number, // "1"
@@ -13,7 +19,7 @@ var userSchema = new Schema({
     lastname: String, // "Holy Ghost"
     patronymic: String, // "Father"
     phone: String, // "+00000000000"
-    e-mail: String, // "creator@this.universe"
+    email: String, // "creator@this.universe"
     picurl: String, // "http://this.universe/creator.png"
     resume: String, // "The Creator"
     birthday: Date // "0000-00-00"
@@ -25,5 +31,54 @@ var userSchema = new Schema({
   }
 });
 
+userSchema.methods.printSummary = function() {
+	console.log('Name: ' + this.name);
+	console.log('Full name: ' + this.personal.firstname + ' ' + this.personal.patronymic + ' ' + this.personal.lastname);
+};
+
 var User = mongoose.model('User', userSchema);
 
+_.series([
+	function (callback) {
+		db.once('open', function() {
+			console.log('Connected to MongoDB');
+			callback();
+		});
+	},
+	function (callback) {
+		console.log('Save new User');
+		var user1 = new User({
+			id: "1",
+			name: "creator",
+			pass: "$2a$12$GRSzwq1wDEOcJhBTcU89HeO.MAQ02tYgO06hyeQcVLaa020lSFb7G",
+			salt: "$2a$12$GRSzwq1wDEOcJhBTcU89He",
+			status: "1",
+			personal:
+			{
+				firstname: "Son",
+				lastname: "Holy Ghost",
+				patronymic: "Father",
+				phone: "+00000000000",
+				email: "creator@this.universe",
+				picurl: "http://this.universe/creator.png",
+				resume: "The Creator",
+				birthday: "0000-00-00"
+			},
+			statistics:
+			{
+				created: "0000-00-00",
+				lastlogin: "2014-09-09"
+			}
+		});
+		console.log("user1 summary:");
+		user1.printSummary();
+		console.log();
+
+		user1.save(function(err, user) {
+			if (err) return console.error(err);
+			console.log('user after saving summary');
+			user.printSummary();
+			callback();
+		});
+	},
+]);

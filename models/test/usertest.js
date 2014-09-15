e459ac6bc02b6476ceab24ebe533e6ef25194d07
Lev Taydakov <lev.taydakov@gmail.com>
2014-09-14 23:31:59 -0700

    draft for testing of User model

---
+++ models/test/usertest.js
@@ -0,0 +1,43 @@
+var user = require('../user');
+var assert = require('assert');
+
+describe('user', function() {
+	beforeEach(function() {
+		// TODO: add some preparation for User model
+	});
+
+	describe('.save(doc)', function() {
+		it('should save the document', function(done) {
+			var pet = { name: 'Tobi' };
+			memdb.save(pet, function() {
+				var ret = memdb.first({ name: 'Tobi' });
+				assert(ret == pet);
+				done();
+			});
+		});
+	});
+
+	describe('.first(obj)', function(){
+		it('should return the first matching doc', function(done) {
+			var tobi = { name: 'Tobi' };
+			var loki = { name: 'Loki' };
+
+			memdb.save(tobi);
+			memdb.save(loki);
+
+			var ret = memdb.first({ name: 'Tobi' });
+			assert(ret == tobi);
+
+			var ret = memdb.first({ name: 'Loki' });
+			assert(ret == loki);
+
+			done();
+		});
+	});
+
+	it('should return null when no doc matches', function(done) {
+		var ret = memdb.first({ name: 'Manny' });
+		assert(ret == null);
+		done();
+	});
+});

8616a9f710d830d9fc2acd8d6675444f6422c2a5
Lev Taydakov <lev.taydakov@gmail.com>
2014-09-25 21:34:50 -0700

    added /warehouse

---
+++ client/app/warehouse/warehouse.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('elnosErpApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('warehouse', {
+        url: '/warehouse',
+        templateUrl: 'app/warehouse/warehouse.html',
+//        controller: 'MainCtrl'
+      });
+  });

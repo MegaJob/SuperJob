e8e580a6b73cedb100a64a718a9fa3d6b316e523
Lev Taydakov <lev.taydakov@gmail.com>
2014-09-27 19:47:10 -0700

    new detail validators implemented

--- client/app/warehouse/newdetail/newdetail.controller.js
+++ client/app/warehouse/newdetail/newdetail.controller.js
@@ -1,6 +1,28 @@
 'use strict';
 
 angular.module('elnosErpApp')
-  .controller('NewDetailCtrl', function ($scope, $http) {
-    $scope.label = "Hello world!";
+  .controller('NewDetailCtrl', function ($scope, $http, $location) {
+  	$scope.errors = {};
+  	$scope.errors.other = {};
+  	$scope.errors.identifier = {};
+  	$scope.errors.name = {};
+
+    $scope.newdetail = function(form) {
+      if (!$scope.detail)
+      	return $scope.errors.other.message = 'You have to fill this form out in order to submit.';
+
+      $http.post('/api/parts', {
+      	identifier: $scope.detail.identifier,
+      	class:      $scope.detail.class,
+      	type:       $scope.detail.type,
+      	name:       $scope.detail.name,
+      	amount:     $scope.detail.amount,
+      	limit:      $scope.detail.limit
+      }).success(function(data) {
+      	$location.path('/warehouse');
+      }).error(function(data) {
+      	if (data.errors)
+	 	  $scope.errors = data.errors;
+      })
+    }
   });

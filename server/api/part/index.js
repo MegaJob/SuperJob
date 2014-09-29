1065bae8279ef7d4efc0944e3f22aa1ccd422cdc
Lev Taydakov <lev.taydakov@gmail.com>
2014-09-28 17:26:07 -0700

    file upload implemented with progress bar

--- server/api/part/index.js
+++ server/api/part/index.js
@@ -2,6 +2,7 @@
 
 var express = require('express');
 var controller = require('./part.controller');
+var multipart = require('connect-multiparty');
 
 var router = express.Router();
 
@@ -11,5 +12,13 @@ router.post('/', controller.create);
 router.put('/:id', controller.update);
 router.patch('/:id', controller.update);
 router.delete('/:id', controller.destroy);
+
+router.post('/uploadpic', multipart({
+    uploadDir: "./tmp"
+  }), function (req, res) {
+	console.log(req.body);
+	console.log(req.files);
+	return res.json(200, req.files);
+});
 
 module.exports = router;
